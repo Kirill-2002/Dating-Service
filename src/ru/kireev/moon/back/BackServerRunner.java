d112048ef6a84d6a44308c9655beb6400c691698
KKireev <lilo662@mail.ru>
2026-08-31 05:01:07 +0300

    Http Server

--- src/ru/kireev/moon/back/BackServerRunner.java
+++ src/ru/kireev/moon/back/BackServerRunner.java
@@ -1,5 +1,9 @@
 package ru.kireev.moon.back;
 
+import ru.kireev.moon.back.controller.ProfileController;
+import ru.kireev.moon.back.dao.ProfileDao;
+import ru.kireev.moon.back.service.ProfileService;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -9,21 +13,10 @@ import java.util.Scanner;
 
 public class BackServerRunner {
     public static  void  main(String[] args) throws IOException {
-        try (ServerSocket serverSocket = new ServerSocket(8080);
-             Socket socket = serverSocket.accept();
-             DataOutputStream responseStream= new DataOutputStream(socket.getOutputStream());
-             DataInputStream requestStream = new DataInputStream(socket.getInputStream());
-             Scanner scanner = new Scanner(System.in)) {
-            while(scanner.hasNextLine()){
-               String request = requestStream.readUTF();
 
-               while(!"stop".equals(request)){
-                   System.out.println("Client request: " + request);
-                   String response = scanner.nextLine();
-                   responseStream.writeUTF(response);
-                   request = requestStream.readUTF();
-               }
-            }
-        }
+        ProfileController profileController = new ProfileController(new ProfileService(new ProfileDao()));
+
+        HttpServer server = new HttpServer(5);
+        server.start();
     }
 }
