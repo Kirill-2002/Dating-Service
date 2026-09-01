52cf907faa3b740caba8b2663a9855d5c66d3ad3
KKireev <lilo662@mail.ru>
2026-09-01 18:44:04 +0300

    Apache Tomcat

---
+++ src/ru/kireev/moon/back/controller/LikeController.java
@@ -0,0 +1,20 @@
+package ru.kireev.moon.back.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/like")
+public class LikeController extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().write("10");
+    }
+}
